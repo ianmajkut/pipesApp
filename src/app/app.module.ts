acb783fc24b204f9ba1a46de802f8f8295d9ae5b
ian <ianedwinmajkut@gmail.com>
2021-11-17 11:53:33 -0300

    Creacion modulo para tener las cosas de primeNg e importacion en app.module. Creacion simples ejemplos y utilizacion dentro del styles.css el fondo, font family, etc a partir de '\node_modules\primeng\resources\themes\vela-blue\theme.css'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+//Modulo personalizado 
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PrimeNgModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

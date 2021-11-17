84e7b8b4578b928a4f0055e955293b532397b0f7
ian <ianedwinmajkut@gmail.com>
2021-11-17 13:38:56 -0300

    Creacion rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRouterModule } from './app-router.module';
 
 
 import { AppComponent } from './app.component';
@@ -14,7 +15,8 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
-    SharedModule
+    SharedModule,
+    AppRouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
